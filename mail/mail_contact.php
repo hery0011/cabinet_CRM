30dfeb7980b84524cec93de7605fd5758f8d12d8
rasolonjatovo hery <rasolonjatovohery5@gmail.com>
2020-12-06 09:49:02 +0300

    login gmail

--- mail/mail_contact.php
+++ mail/mail_contact.php
@@ -14,12 +14,11 @@ try {
 
     //Server settings
 	$mail->isSMTP();
-	// $mail->Host = "smtp.gmail.com";
-	$mail->Host = "mail.crmcac.com";
+	$mail->Host = "smtp.gmail.com";
 	$mail->SMTPAuth = true;
-	$mail->Username = "crmcac@crmcac.com";
-	$mail->Password = 'Crmcac@123';
-	$mail->Port = 587;
+	$mail->Username = "rasolonjatovohery0011@gmail.com";
+	$mail->Password = 'hery0011';
+	$mail->Port = 465;
 	$mail->SMTPSecure = "ssl";
 
 	//email setting
